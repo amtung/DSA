f3575aa316bfb58f030726535a869c8d2422096e
Annie <annie.mmt@gmail.com>
2017-04-26 15:28:00 -0400

    invert a binary search tree

--- DSA problems.playground/Pages/Trees.xcplaygroundpage/Contents.swift
+++ DSA problems.playground/Pages/Trees.xcplaygroundpage/Contents.swift
@@ -41,6 +41,30 @@ class TreeNode {
 //        }
 //        return sum
 //    }
+    
+    // Invert a BST: https://leetcode.com/problems/invert-binary-tree/#/description
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        guard let unwrappedRoot = root else {
+            return nil
+        }
+        invertTreeRecursively(root!)
+        return root
+    }
+    func invertTreeRecursively(_ node: TreeNode) {
+        swapChildren(node)
+        if node.left != nil {
+            invertTreeRecursively(node.left!)
+        }
+        if node.right != nil {
+            invertTreeRecursively(node.right!)
+        }
+    }
+    func swapChildren(_ root: TreeNode) {
+        let left = root.left
+        let right = root.right
+        root.left = right
+        root.right = left
+    }
 }
 
 //: [Next](@next)
