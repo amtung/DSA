67227d935e1e36929e703038d5958dc503117d66
Annie <annie.mmt@gmail.com>
2017-05-26 15:23:43 -0400

    search whether the usage of capitals is in the word

--- DSA problems.playground/Pages/Strings.xcplaygroundpage/Contents.swift
+++ DSA problems.playground/Pages/Strings.xcplaygroundpage/Contents.swift
@@ -197,3 +197,36 @@ func isPalindrome(_ s: String) -> Bool {
 isPalindrome("A man, a plan, a canal: Panama")
 isPalindrome("race a car")
 isPalindrome("tacocat")
+
+/* Given a word, you need to judge whether the usage of capitals in it is right or not. We define the usage of capitals in a word to be right when one of the following cases holds:
+ All letters in this word are capitals, like "USA".
+ All letters in this word are not capitals, like "leetcode".
+ Only the first letter in this word is capital if it has more than one letter, like "Google".
+ Otherwise, we define that this word doesn't use capitals in a right way.
+ Example:
+ Input: "USA"
+ Output: True
+ 
+ Input: "FlaG"
+ Output: False*/
+func detectCapitalUse(_ word: String) -> Bool {
+    return word == word.uppercased() || word == word.lowercased() || word == word.capitalized
+}
+
+extension String {
+    
+    func isUppercase() -> Bool {
+        return self.characters.reduce(true){$0 && CharacterSet.uppercaseLetters.contains(UnicodeScalar(String($1))!)}
+    } // CharacterSet = {"A","B","C"...}
+    
+    func isLowercase() -> Bool {
+        return self.characters.reduce(true){$0 && CharacterSet.lowercaseLetters.contains(UnicodeScalar(String($1))!)}
+    }
+}
+"USA".isUppercase()
+"USA".isLowercase()
+"FlaG".isUppercase()
+"Google".isUppercase()
+detectCapitalUse("Google")
+detectCapitalUse("leetcode")
+detectCapitalUse("FlaG")
