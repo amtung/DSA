440256d81eee06b6aae5d66109d7abb2a2933c33
Annie <annie.mmt@gmail.com>
2017-06-04 21:11:02 -0400

    recursion practice

---
+++ DSA problems.playground/Pages/Recursion.xcplaygroundpage/Contents.swift
@@ -0,0 +1,66 @@
+//: [Previous](@previous)
+
+import Foundation
+// https://github.com/amtung/web-technical-interviewing
+
+// Write a function recursiveSum that receives a positive number n and returns the sum of all numbers from 0...n.
+func addSum(num: Int) -> Int {
+    if num == 0 {
+        return 0
+    }
+    return num + addSum(num: num - 1)
+}
+print(addSum(num: 7))
+
+// Write a function recursiveMultiply that receives a positive number n and returns the sum of all numbers from 0...n.
+func findMultiple(a: Int, b: Int) -> Int {
+    guard b > 0 else { return 0 }
+    return a + findMultiple(a: a, b: b - 1)
+}
+print(findMultiple(a: 3, b: 3))
+
+// Write a function factorial that receives a number n and recursively calculates and returns n!.
+func findFactorial(num: Int) -> Int {
+    if num == 1 {
+        return 1
+    }
+    return num * findFactorial(num: num  - 1)
+}
+print(findFactorial(num: 3))
+
+// Write a recursive function gcf that receives two numbers and returns the greatest common factor of those two numbers.
+func findGCF(a: Int, b: Int) -> Int {
+    if a > b {
+        return b
+    }
+    findGCF(a: a, b: a % b)
+    return 0
+}
+
+// Write a function subsets that receives an array and returns all the possible subsets that can be made from the elements in the array.
+func findSubsets(arr: [Int], emptyArr: [[Int]]) -> [[Int]] {
+    guard arr.count != 0 else { return emptyArr }
+    var finalArr = emptyArr
+    
+    var myArr = arr
+    var firstNum = myArr.removeFirst()
+    
+    let addedArr = finalArr.map{$0 + [firstNum]}
+    print(addedArr)
+
+    return findSubsets(arr: myArr, emptyArr: finalArr + addedArr)
+}
+print(findSubsets(arr: [1,2,3], emptyArr: [[]]))
+
+// Write a function findPerms that receives a string and returns an array with all the permutations that can be made from the characters in the string.
+func findPerm(str: String) -> [String] {
+    var returnArr = [""]
+    //    var stringABC = "abc"
+    //    var rest = stringABC.remove(at: 0)
+    //    print(rest)
+    return returnArr
+}
+
+
+
+//: [Next](@next)
